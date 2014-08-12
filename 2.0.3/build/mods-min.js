7589bcb8d6de9c570e8bbf48f2869d326f0faf74
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-12 10:54:08 +0800

    sync daily version 2.0.3

---
+++ 2.0.3/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-08-12 10:53:13 AM*/
+KISSY.config("modules",{"kg/departurelayer/2.0.3/toptip":{requires:["base","node"]}});
---
+++ 2.0.3/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/departurelayer/2.0.3/toptip': { requires: ['base', 'node']} 
+});
