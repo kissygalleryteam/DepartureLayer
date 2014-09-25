a351236f5472e205133cd13a5b171d1c8278d90b
弘树 <tiehang.lth@alibaba-inc.com>
2014-09-26 01:10:43 +0800

    sync daily branch 2.0.5

--- 2.0.2/build/mods-min.js
+++
@@ -1,2 +0,0 @@
-/*!build time : 2014-08-06 2:04:03 PM*/
-KISSY.config("modules",{"kg/departurelayer/2.0.1/toptip":{requires:["base","node"]}});
--- 2.0.2/meta/alias.js
+++
@@ -1,3 +0,0 @@
-config({
-    'kg/depatureie6/index': {alias: ['kg/depatureie6/2.0.0/index']}
-});
